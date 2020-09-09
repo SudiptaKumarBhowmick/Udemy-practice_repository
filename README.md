5eff98aceb0c71043880368be2ba3e98abfb3dd9
reposudipta <shuvo.archies85@gmail.com>
2020-09-09 18:20:55 +0600

    initial commit done

---
+++ README.md
@@ -0,0 +1,2 @@
+#This is Udemy Practice File
+## This file is created by Sudipta Kumar Bhowmick
---
+++ src/API/Controllers/DepartmentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route(template: "[Controller]")]
+    public class DepartmentController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Get ALl Student");
+        }
+
+        [HttpGet(template: "{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("Get this " + code + " department data.");
+        }
+
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("Insert new department.");
+        }
+
+        [HttpPut(template: "{code}")]
+        public IActionResult Update(string code)
+        {
+            return Ok("Update this " + code + " department data.");
+        }
+
+        [HttpDelete(template: "{code}")]
+        public IActionResult Delete(string code)
+        {
+            return Ok("Delete this " + code + "department data.");
+        }
+    }
+}
